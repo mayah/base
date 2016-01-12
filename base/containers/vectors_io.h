1aaa2ed1208b6cdb9bb3859e04ae0643a23ccf6e
MAYAH <mayah@mayah.jp>
2016-01-12 19:33:53 +0900

    buildfix: Remove unused variable.

--- base/containers/vectors_io.h
+++ base/containers/vectors_io.h
@@ -7,7 +7,6 @@
 template<typename T>
 inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vs)
 {
-    bool first = true;
     for (size_t i = 0; i < vs.size(); ++i) {
         if (i > 0)
             os << ' ';
