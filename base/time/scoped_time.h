bb514865bc24224381a37aa4935d71975d53e1f2
MAYAH <mayah@mayah.jp>
2016-01-21 22:10:45 +0900

    Add base::ScopedTime.

---
+++ base/time/scoped_time.h
@@ -0,0 +1,31 @@
+#ifndef BASE_TIME_SCOPED_TIME_H_
+#define BASE_TIME_SCOPED_TIME_H_
+
+#include "base/time/current_time.h"
+
+namespace base {
+
+class ScopedTime {
+public:
+    explicit ScopedTime(double* sec);
+    ~ScopedTime();
+
+private:
+    double t_;
+    double* sec_;
+};
+
+inline ScopedTime::ScopedTime(double* sec) :
+    t_(current_time()),
+    sec_(sec)
+{
+}
+
+inline ScopedTime::~ScopedTime()
+{
+    *sec_ = current_time() - t_;
+}
+
+} // namespace base
+
+#endif // BASE_TIME_SCOPED_TIME_H_
--- example/current_time.cc
+++ example/current_time.cc
@@ -1,13 +1,34 @@
 #include <iostream>
 
 #include "base/time/clock.h"
+#include "base/time/scoped_time.h"
 #include "base/time/time.h"
 
 using namespace std;
+
+int fib(int n)
+{
+    if (n <= 2)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+void f()
+{
+    double d;
+    {
+        base::ScopedTime st(&d);
+        cout << fib(42) << endl;
+    }
+    cout << d << endl;
+}
 
 int main()
 {
     base::Time t = base::Clock::real_clock()->now();
     time_t now = t.to_time_t();
     cout << ctime(&now) << endl;
+
+    f();
+    return 0;
 }
