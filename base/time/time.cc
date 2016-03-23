b3da9755f5ee23824427770087e194d2438116e8
MAYAH <mayah@mayah.jp>
2016-03-23 11:25:23 +0900

    Don't include LF in localtime_string.

--- base/time/time.cc
+++ base/time/time.cc
@@ -1,5 +1,7 @@
 #include "base/time/time.h"
 
+#include <string.h>
+
 namespace base {
 
 std::string Time::to_localtime_string() const
@@ -8,7 +10,12 @@ std::string Time::to_localtime_string() const
     time_t t = to_time_t();
     ctime_r(&t, buf);
 
-    return buf;
+    // Drop the last \n from buf.
+    size_t len = strlen(buf);
+    if (len == 0)
+        return std::string();
+
+    return std::string(buf, buf + (len - 1));
 }
 
 }
