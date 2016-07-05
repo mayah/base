833dd22939c34cd1afdba53454d7365ebd25ed00
mayah <mayah@mayah.jp>
2016-07-05 13:25:49 +0900

    code

--- rust/lib/scanner/src/lib.rs
+++ rust/lib/scanner/src/lib.rs
@@ -1,17 +1,46 @@
 use std::io;
+use std::collections::VecDeque;
 
-pub struct Scanner<R> {
+#[allow(dead_code)]
+pub struct Scanner<R: io::Read> {
     r: R,
+
+    // buffer. Good if we could use the internal buffer of io::BufRead...
+    buf: VecDeque<u8>,
 }
 
-impl<R: io::BufRead> Scanner<io::Chars<R>> {
-    fn new(reader: R) -> Scanner<io::Chars<R>> {
+impl<R: io::Read> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
         Scanner {
-            r: reader.chars()
+            r: reader,
+            buf: VecDeque::new(),
         }
     }
+
+/*
+    /// Returns next character. If EOF, None is returned.
+    fn next_char() -> Option<u8> {
+        if buf.is_empty() {
+    if !fill() {
+    return Err(Nothing
+}
+}
+    }
+*/
+
+/*
+    fn next_int() -> Result<int, Error> {
+        let mut sign = 1;
+        match next_char() {
+            Ok('+') => sign = 1,
+            Ok('-') => sign = -1,
+            Ok(c) => unputc(c),
+            Err(x) => return Err(x)
+        }
+    }
+*/
 }
 
-pub fn from_stdin() -> Scanner<io::Chars<io::Stdin>> {
+pub fn from_stdin() -> Scanner<io::Stdin> {
     Scanner::new(io::stdin())
 }
