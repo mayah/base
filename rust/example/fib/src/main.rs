69776338b277006350f6d1f749d879ca5f5742d8
mayah <mayah@mayah.jp>
2016-07-06 10:49:26 +0900

    Fix rust indentation.

--- rust/example/fib/src/main.rs
+++ rust/example/fib/src/main.rs
@@ -1,12 +1,12 @@
 fn fib(n: i32) -> i32 {
-  match n {
-    0 | 1 => 1,
-    _ => fib(n - 1) + fib(n - 2),
-  }
+    match n {
+        0 | 1 => 1,
+        _ => fib(n - 1) + fib(n - 2),
+    }
 }
 
 fn main() {
-  for n in 0..40 {
-    println!("fib({}) = {}", n, fib(n))
-  }
+    for n in 0..40 {
+        println!("fib({}) = {}", n, fib(n))
+    }
 }
